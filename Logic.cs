e83fa1db19da3909014ce65b609e6d8412b27ce2
Maluska10 <salamona@csp.edu>
2025-06-03 22:27:19 -0500

    Add project files.

---
+++ Logic.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    class Logic
+    {
+
+        public const int COLUMNS = 3;
+        public const int ROWS = 3;
+
+        public static string[,] grid =
+          {
+                { " ", " ", " " },
+                { " ", " ", " " },
+                { " ", " ", " " }
+            };
+        public static void PrintGrid(string[,] grid)
+        {
+            for (int j = 0; j < COLUMNS; j++)
+            {
+                Console.Write(" ---", Color.Green);
+            }
+
+            Console.Write("\n");
+
+            for (int i = 0; i < ROWS; i++)
+            {
+                if (i > 0)
+                {
+                    for (int j = 0; j < COLUMNS; j++)
+                    {
+                        Console.Write(" ---", Color.Red);
+
+                    }
+                    Console.WriteLine();
+                }
+
+                for (int j = 0; j < COLUMNS; j++)
+                {
+                    Console.Write("|" + "  " + " ", Color.Red);
+                }
+                Console.WriteLine("|", Color.Red);
+            }
+
+            for (int j = 0; j < COLUMNS; j++)
+            {
+                Console.Write(" ---", Color.Green);
+            }
+        }
+
+        
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Drawing;
+using Console = Colorful.Console;
+
+namespace TicTacToe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string SYMBOL_CHOICE_O = "O";
+            const string SYMBOL_CHOICE_X = "X";
+            string columnChoice = "0";
+            string rowChoice = "0";
+
+            Console.WriteLine("Welcome to Tic-Tac-Toe! Let's play!");
+            Console.WriteLine();
+            Console.WriteLine("What symbol would you like to be? \nPress O or X.");
+            string symbolChoice = Console.ReadLine()?.ToUpper();
+
+            while (symbolChoice != SYMBOL_CHOICE_O && symbolChoice != SYMBOL_CHOICE_X)
+            {
+                Console.WriteLine("Incorrect input. Please try again.");
+                symbolChoice = Console.ReadLine()?.ToUpper();
+            }
+
+            //grid printout
+            Logic.PrintGrid(Logic.grid);
+            Console.WriteLine();
+
+
+            while (true)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Where would you like to place the {symbolChoice}?");
+                Console.WriteLine("Which column? Type 0, 1 or 2.");
+                columnChoice = Console.ReadLine();
+                Console.WriteLine("Which row? Type 0, 1 or 2.");
+                rowChoice = Console.ReadLine();
+
+                if (int.TryParse(columnChoice, out int column) && int.TryParse(rowChoice, out int row))
+                    if (row >= 0 && row < 3 && column >= 0 && column < 3)
+                    {
+                        Logic.grid[row, column] = symbolChoice;
+                        Logic.PrintGrid(Logic.grid);
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid spot. Try again.");
+                        Console.ReadLine();
+                    }
+
+                //AI placing a symbol 
+
+
+
+            }
+
+            }
+        }
+    }
